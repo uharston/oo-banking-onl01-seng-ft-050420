1b0916fe269261b3f0834a499717b8b68087dce4
Uriah Harston <uharston@gmail.com>
2020-05-20 15:13:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
 
+  attr_reader :name
+  attr_accessor  :status, :balance
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+  #  @balance > 0  ? true : false && @status == 'open' ? true : false
+     if @balance <= 0
+       false
+     elsif @status == 'closed'
+         false
+     else
+       true
+     end
+   end
+
+   def close_account
+     @status = 'closed'
+   end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
-  # your code here
-end
+
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    sender.valid?
+    receiver.valid?
+  end
+
+  # def execute_transaction
+  #     if @sender.balance > @amount && @status == 'pending' && self.valid?
+  #        @sender.balance -= @amount
+  #        @receiver.balance += @amount
+  #        @status = 'complete'
+  #      else
+  #        @status = "Transaction rejected. Please check your account balance."
+  #     end
+  #
+  # end
+  def execute_transaction
+    if sender.balance > amount && status == 'pending' && self.valid?
+       sender.balance -= amount
+       receiver.balance += amount
+       @status = 'complete'
+    elsif
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+     end
+  end
+ end
